a5a0003235d55a407ecae81b597e6ef234e8f4f0
Noah <Noah580@users.noreply.github.com>
2025-01-12 17:57:11 +0100

    fichier sidebar avec un html css et js

---
+++ Sidebar/index.js
@@ -0,0 +1,7 @@
+// side bar ouverture 
+
+document.querySelector('i').addEventListener('click', ()=>{
+    const sidebar = document.querySelector('.sidebar')
+
+    sidebar.classList.toggle('active')
+})
